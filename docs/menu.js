153554e5a11dee65937f101822a9ae03f647f722
Antonio Gallo <agx@linux.it>
2019-01-07 21:29:03 +0100

    initial release with some docs

---
+++ docs/menu.js
@@ -0,0 +1,17 @@
+
+document.write('<nav id="docmenu"><ul>\n' +
+	'<li><a href="typography.html" >Typography</a></li>\n' +
+	'<li><a href="images.html" >Images</a></li>\n' +
+	'<li><a href="buttons.html" >Buttons</a></li>\n' +
+/*TODO	'<li><a href="forms.html" >Forms</a> </li>\n' +*/
+	'<li><a href="grid-basic.html" >Grid: basic</a></li>\n' +
+	'<li><a href="grid-nested.html" >Grid: nested</a></li>\n' +
+	'<li><a href="grid-gutter.html" >Grid: gutter</a></li>\n' +
+	'<li><a href="viewport.html" >Viewport</a></li>\n' +
+	//'<li><a href="cards.html" >cards</a></li>\n' +
+	'<li><a href="fab.html" >FAB</a></li>\n' +
+	'<li><a href="overlay.html" >overlay</a></li>\n' +
+	'<li><a href="animate.html" >animate</a></li>\n' +
+	//'<li><a href="navbar.html" >navbar</a></li>\n' +
+	'</ul></nav>'
+);
