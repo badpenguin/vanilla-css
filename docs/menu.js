039647f1a3f7e0055e120b796a1ad13f50309603
Antonio Gallo <agx@linux.it>
2019-06-30 19:39:42 +0200

    - converted the demo menu to the new navbar - fixed some bugs in the demo - changed navbar overlay close's icon - added padding to containers when fluids

--- docs/menu.js
+++ docs/menu.js
@@ -1,16 +1,43 @@
-document.write('<nav class="docmenu"><ol>\n' +
-	'<li class="home"><a href="index.html" >Vanilla CSS</a></li>\n' +
-	'<li><a href="10.typography.html" >Typography</a></li>\n' +
-	'<li><a href="20.images.html" >Images</a></li>\n' +
-	'<li><a href="30.buttons.html" >Buttons</a></li>\n' +
-	'<li><a href="forms.html" >Forms</a> </li>\n' +
-	'<li><a href="40.grid-basic.html" >Grid: basic</a></li>\n' +
-	'<li><a href="41.grid-nested.html" >Grid: nested</a></li>\n' +
-	'<li><a href="42.grid-gutter.html" >Grid: gutter</a></li>\n' +
-	'<li><a href="50.viewport.html" >Viewport</a></li>\n' +
-	'<li><a href="60.fab.html" >FAB</a></li>\n' +
-	'<li><a href="70.overlay.html" >overlay</a></li>\n' +
-	'<li><a href="80.animate.html" >animate</a></li>\n' +
-	'<li><a href="navbar.html" >navbar</a></li>\n' +
-	'</ol></nav>'
-);
+var menu = [
+	['index.html', 'Vanilla CSS', 'home vanilla-version dont-toggle'],
+	['10.typography.html', 'Typography', ''],
+	['20.images.html', 'Images'],
+	['30.buttons.html', 'Buttons'],
+	['forms.html', 'Forms'],
+	['40.grid-basic.html', 'Grid: basic'],
+	['41.grid-nested.html', 'Grid: nested'],
+	['42.grid-gutter.html', 'Grid: gutter'],
+	['50.viewport.html', 'Viewport'],
+	['60.fab.html', 'FAB'],
+	['70.overlay.html', 'overlay'],
+	['80.animate.html', 'animate'],
+	['navbar.html', 'navbar']
+];
+
+
+function writeResponsiveMenu() {
+	document.write('<nav class="navbar"><ul class="navbar-menu scrollable flex-center navbar-menu-left">');
+
+	[].forEach.call(menu, function (item) {
+		document.write('<li class="' + item[2] + '"><a href="' + item[0] + '">' + item[1] + '</a></li>');
+	});
+
+	document.write('</ul><div class="navbar-toggle"><label for="navbar-toggle">&#9776; Menu</label><input type="checkbox" id="navbar-toggle"/><div class="navbar-overlay"><label class="navbar-overlay-close" for="navbar-toggle">&times;</label><ul>');
+
+	[].forEach.call(menu, function (item) {
+		document.write('<li class="' + item[2] + '"><a href="' + item[0] + '">' + item[1] + '</a></li>');
+	});
+
+	document.write('</ul></div></div></nav>');
+
+}
+
+
+function writeSimpleMenu() {
+
+	document.write('<nav class="docmenu"><ol>');
+	[].forEach.call(menu, function (item) {
+		document.write('<li class="' + item[2] + '"><a href="' + item[0] + '">' + item[1] + '</a></li>');
+	});
+	document.write('</ol></nav>');
+}
