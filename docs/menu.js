b8a4b4963ecb38b8b886cfa321d030f4091036e5
Antonio Gallo <agx@linux.it>
2019-06-30 22:16:36 +0200

    bug fixing

--- docs/menu.js
+++ docs/menu.js
@@ -1,17 +1,17 @@
 var menu = [
 	['index.html', 'Vanilla CSS', 'home vanilla-version dont-toggle'],
 	['10.typography.html', 'Typography', ''],
-	['20.images.html', 'Images'],
-	['30.buttons.html', 'Buttons'],
-	['forms.html', 'Forms'],
-	['40.grid-basic.html', 'Grid: basic'],
-	['41.grid-nested.html', 'Grid: nested'],
-	['42.grid-gutter.html', 'Grid: gutter'],
-	['50.viewport.html', 'Viewport'],
-	['60.fab.html', 'FAB'],
-	['70.overlay.html', 'overlay'],
-	['80.animate.html', 'animate'],
-	['navbar.html', 'navbar']
+	['20.images.html', 'Images', ''],
+	['30.buttons.html', 'Buttons', ''],
+	['forms.html', 'Forms', ''],
+	['40.grid-basic.html', 'Grid: basic', ''],
+	['41.grid-nested.html', 'Grid: nested', ''],
+	['42.grid-gutter.html', 'Grid: gutter', ''],
+	['50.viewport.html', 'Viewport', ''],
+	['60.fab.html', 'FAB', ''],
+	['70.overlay.html', 'overlay', ''],
+	['80.animate.html', 'animate', ''],
+	['navbar.html', 'navbar', ''],
 ];
 
 
