0e68e0262a20645c93025efeb5319b45f9d8f92e
Antonio Gallo <agx@linux.it>
2020-07-02 14:59:29 +0200

    completed the refactoring using BEM

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Its written in SASS so you can use all the files or just select the one that you
 Its currently used on https://antoniogallo.it/ and https://visitaretenerife.it/
 
 DEMO: https://badpenguin.github.io/vanilla-css/
+
+
+## Building
+
+I use "Web/PHP Storm" with watchers to build the project with:
+
+```scss --style compressed --update $FileName$:../docs/$FileNameWithoutExtension$.min.css```
+
+
+## Generators
+
+* Angled edges: https://angled-edges-generator.josephfus.co/
--- docs/menu.js
+++ docs/menu.js
@@ -1,35 +1,49 @@
 var menu = [
 	['index.html', 'Vanilla CSS', 'home dont-toggle'],
-	['https://github.com/badpenguin/vanilla-css/','GitHub','text-bold'],
 	['10.typography.html', 'Typography', ''],
-	['11.medium.html', 'Medium.Com', ''],
+	['11.article.html', 'Article', ''],
 	['20.images.html', 'Image', ''],
 	['30.buttons.html', 'Button', ''],
 	['35.forms.html', 'Form', ''],
-	['40.grid-basic.html', 'Grid: basic', ''],
-	['41.grid-nested.html', 'Grid: nested', ''],
-	['42.grid-gutter.html', 'Grid: gutter', ''],
+
+	['#', 'Grid', '',
+		[
+			['40.grid-basic.html', 'Grid: basic', ''],
+			['41.grid-nested.html', 'Grid: nested', ''],
+			['42.grid-gutter.html', 'Grid: gutter', ''],
+		]
+	],
 	['50.viewport.html', 'Viewport', ''],
 	['60.fab.html', 'FAB', ''],
 	['65.cards.html', 'Card', ''],
 	['70.overlay.html', 'Overlay', ''],
 	['80.animate.html', 'Animate', ''],
 	['90.navbar.html', 'Navbar', ''],
+	['91.wordpress.html', 'Wordpress', ''],
+	['https://github.com/badpenguin/vanilla-css/', 'GitHub', 'text-bold'],
 ];
 
 
 function writeResponsiveMenu() {
 	var html = '';
-	html += '<header><nav class="navbar navbar-toggle-on-medium"><ul class="navbar-menu scrollable navbar-menu-left">';
+	html += '<header id="mainmenu"><nav class="navbar navbar--toggle-always navbar--toggle-on-medium"><ul class="navbar__menu navbar__menu--left">';
 
 	[].forEach.call(menu, function (item) {
-		html += '<li class="' + item[2] + '"><a href="' + item[0] + '">' + item[1] + '</a></li>';
+		if (item[3]) {
+			html += '<li class="hover-overlay ' + item[2] + '"><a href="' + item[0] + '">' + item[1] + '</a><ul class="hover-inner">';
+			[].forEach.call(item[3], function (subitem) {
+				html += '<li class="' + subitem[2] + '"><a href="' + subitem[0] + '">' + subitem[1] + '</a></li>';
+			});
+			html += '</ul></li>';
+		} else {
+			html += '<li class="' + item[2] + '"><a href="' + item[0] + '">' + item[1] + '</a></li>';
+		}
 	});
 
 	html +=
-	'</ul><div class="navbar-toggle"><label for="navbar-toggle">&#9776;&nbsp;Menu</label>' +
-		'<input type="checkbox" id="navbar-toggle"/><div class="navbar-overlay navbar-overlay__right">' +
-		'<label class="navbar-overlay-close" for="navbar-toggle">&times;</label><ul>'
+		'</ul><div class="navbar__toggle"><label for="navbar-toggle">&#9776;&nbsp;Menu</label>' +
+		'<input type="checkbox" id="navbar-toggle"/><div class="navbar__overlay navbar__overlay--right">' +
+		'<label class="navbar__overlay__close" for="navbar-toggle">&times;</label><ul>'
 	;
 
 	[].forEach.call(menu, function (item) {
@@ -41,9 +55,9 @@ function writeResponsiveMenu() {
 }
 
 
-onPageReady(function(){
+$onReady(function () {
 
-	document.body.insertAdjacentHTML('afterbegin', writeResponsiveMenu() );
+	document.body.insertAdjacentHTML('afterbegin', writeResponsiveMenu());
 
-	$append('<footer><a href="https://github.com/badpenguin/vanilla-css/">Download from GitHub</a><a href="#" class="back-to-top">Back to Top</a></footer>');
+	$appendBody('<footer><a href="https://github.com/badpenguin/vanilla-css/">Download from GitHub</a><a href="#" class="back-to-top">Back to Top</a></footer>');
 });
